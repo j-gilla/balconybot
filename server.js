2ae223d19f976a7427476c8c01a366841db87288
gilbertjacke <gilbertjacke@gmail.com>
2017-07-14 11:54:51 +0100

    added setinterval

--- server.js
+++ server.js
@@ -3,62 +3,60 @@ const path = require('path');
 const Twit = require('Twit');
 const config = require(path.join(__dirname, 'config.js'));
 
-const tweet = new Twit(config);
+const T = new Twit(config);
 
-function pickRandomImage(){
-  let images = [
-    'one.jpg',
-    'two.jpg',
-    'three.jpg',
-    'four.jpg'
-  ];
+function random_from_array(images){
   return images[Math.floor(Math.random() * images.length)];
-};
+}
 
-
-
-function uploadRandomImage(){
-  console.log('Opening image...')
-  let imagePath = path.join(__dirname, '/assets/' + pickRandomImage()),
-  b64content = fs.readFileSync(imagePath, { encoding: 'base64' });
+function upload_random_image(images){
+  console.log('Opening an image...');
+  var image_path = path.join(__dirname, '/images/' + random_from_array(images)),
+      b64content = fs.readFileSync(image_path, { encoding: 'base64' });
 
   console.log('Uploading an image...');
 
-  tweet.post('media/upload', { media_data: b64content }, function (err, data, response) {
-    if(err) {
-      console.log('ERROR');
+  T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+    if (err){
+      console.log('ERROR:');
       console.log(err);
     }
     else{
-      console.log('Uploaded an image!');
+      console.log('Image uploaded!');
+      console.log('Now tweeting it...');
 
-      tweet.post('statuses/update', {
-        status: new Array(data)
+      T.post('statuses/update', {
+
+        media_ids: new Array(data.media_id_string)
       },
-      function(err, data, response) {
-        if (err) {
-          console.log("Final error");
-          console.log(err);
-        } else {
-          console.log("Posted an image")
+        function(err, data, response) {
+          if (err){
+            console.log('ERROR:');
+            console.log(err);
+          }
+          else{
+            console.log('Posted an image!');
+          }
         }
-      }
-    );
-  }
-});
+      );
+    }
+  });
 }
 
-setInterval(
-  uploadRandomImage,
-  10000
-);
 
+fs.readdir(__dirname + '/images', function(err, files) {
+  if (err){
+    console.log(err);
+  }
+  else{
+    var images = [];
+    files.forEach(function(f) {
+      images.push(f);
+    });
 
+    setInterval(function(){
+      upload_random_image(images);
+    }, 10000);
 
-
-
-//
-
-// tweet.post('statuses/update', { status: 'All watched over by balconies of love and grace'}, function(err, data,response){
-//   console.log(data);
-// });
+  }
+});
